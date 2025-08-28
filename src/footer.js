49d1caad6970fa1f56fbf49e6da95f32e95aa609
Hayden Freeman <mail@haydfree.com>
2025-08-28 12:27:25 -0500

    mono better

--- src/footer.js
+++
@@ -1,9 +0,0 @@
-class FooterT extends HTMLElement {
-    async connectedCallback() {
-        const res = await fetch("/components/footer.html");
-        const html = await res.text();
-        this.innerHTML = html;
-    }
-}
-
-customElements.define("footer-t", FooterT);
