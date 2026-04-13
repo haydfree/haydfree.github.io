55d7f1f69137f02c74eecdfcf521bf85e1f4f04d
Hayden Freeman <mail@haydfree.com>
2026-04-13 16:39:23 -0500

    look at what they did to ma boi

--- src/components.js
+++
@@ -1,3 +0,0 @@
-import("/src/nav.js");
-import("/src/contact.js");
-import("/src/footer.js");
--- src/contact.js
+++
@@ -1,9 +0,0 @@
-class ContactT extends HTMLElement {
-    async connectedCallback() {
-        const res = await fetch("/components/contact.html");
-        const html = await res.text();
-        this.innerHTML = html;
-    }
-}
-
-customElements.define("contact-t", ContactT);
